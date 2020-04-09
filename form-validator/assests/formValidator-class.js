4492d8248ea22bce0a2ed7dc1031875f98e5bda3
Lucas dos Prazeres <olucascosta@outlook.com>
2020-04-09 20:41:13 -0300

    empty field validation was almost concluted

--- form-validator/assests/formValidator-class.js
+++ form-validator/assests/formValidator-class.js
@@ -36,30 +36,37 @@ class Form {
 
     static setErrorMessage(element, errorMsg) {
         const errorDiv = document.createElement('div');
-
         errorDiv.innerText = errorMsg;
+        errorDiv.classList.add('err');
         element.appendChild(errorDiv);
         console.log(errorDiv);
     };
 
 };
 
-const nameField = document.querySelector('#nome');
-const lastNameField = document.querySelector('#sobrenome');
-const CpfField = document.querySelector('#cpf');
-const userField = document.querySelector('#user');
-const passwordField = document.querySelector('#senha');
-const confirmPasswordField = document.querySelector('#senha-confirm');
 const button = document.querySelector('.btn-enviar');
-
-const nameDiv = document.querySelector('.input-group');
-
-console.log(button);
+const nameGroup = document.querySelector('.name-group');
+const lastNameGroup = document.querySelector('.lastname-group');
+const userGroup = document.querySelector('.user-group');
+const passwordGroup = document.querySelector('.password-group');
 
 //validating 'nome'
 button.addEventListener('click', function (e) {
     e.preventDefault();
 
-    if (!Form.validateRegularField(nameField.value))
-        Form.setErrorMessage(nameDiv, "Você deve preencher todos os campos!");
+    if (!Form.validateRegularField(nameGroup.querySelector('#nome').value)) {
+        Form.setErrorMessage(nameGroup, "Você deve preencher todos os campos!");
+    }
+
+    if (!Form.validateRegularField(lastNameGroup.querySelector('#sobrenome').value)) {
+        Form.setErrorMessage(lastNameGroup, "Você deve preencher todos os campos!");
+    }
+
+    if (!Form.validateUserField(userGroup.querySelector('#user').value)) {
+        Form.setErrorMessage(userGroup, "Você deve preencher todos os campos!");
+    }
+
+    if (!Form.validatePasswordField(passwordGroup.querySelector('#senha').value)) {
+        Form.setErrorMessage(passwordGroup, "Você deve preencher todos os campos!");
+    }
 });
