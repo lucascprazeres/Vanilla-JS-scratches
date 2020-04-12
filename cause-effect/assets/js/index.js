d4644f0260440114096d613f93465a31036e5bc8
Lucas dos Prazeres <olucascosta@outlook.com>
2020-04-12 11:56:36 -0300

    user stories were all done and the first versions is ready

--- cause-effect/assets/js/index.js
+++ cause-effect/assets/js/index.js
@@ -1,9 +1,23 @@
 function openNav() {
     document.getElementsByClassName("sidenav")[0].style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
+    document.getElementsByClassName("fichas")[0].style.marginLeft = "200px";
 }
 
 function closeNav() {
     document.getElementsByClassName("sidenav")[0].style.width = "0";
-    document.getElementById("main").style.marginLeft = "0"
+    document.getElementsByClassName("fichas")[0].style.marginLeft = "0";
+}
+
+function showProfile(name) {
+    refreshPane();
+    const profile = document.getElementsByClassName(name)[0];
+    document.querySelector(`#${profile.className}`).classList.add("btn-selected");
+    profile.style.display = "block";
+}
+
+function refreshPane() {
+    for (let profile of document.querySelectorAll('fieldset')) {
+        profile.style.display = "none";
+        document.querySelector(`#${profile.className}`).classList.remove("btn-selected");
+    }
 }
